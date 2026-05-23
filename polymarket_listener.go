bd90cfd360fffd6464efddd75c1279c610642826
友人 <bizi@yourens-MacBook-Air.local>
2026-05-23 10:20:29 +0800

    Polymarket改用Infura WS（与巨鲸共享Infura，免费版支持3WS并发）

--- polymarket_listener.go
+++ polymarket_listener.go
@@ -42,13 +42,13 @@ func startPolymarketListener() {
 }
 
 func runPolymarketListener() error {
-	wsClient, err := ethclient.Dial("wss://1rpc.io/matic")
+	wsClient, err := ethclient.Dial(config.WsRpcUrl)
 	if err != nil {
 		return fmt.Errorf("polymarket ws: %w", err)
 	}
 	defer wsClient.Close()
 
-	httpClient, err := ethclient.Dial("https://1rpc.io/matic")
+	httpClient, err := ethclient.Dial(config.HttpRpcUrl)
 	if err != nil {
 		return fmt.Errorf("polymarket http: %w", err)
 	}
